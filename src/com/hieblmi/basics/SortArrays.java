5fda843ca6e8415e6869a347f6b102019be1b79d
Michael Hiebl <hieblmi@gmail.com>
2017-08-04 23:07:21 -0400

    Sort three arrays - not quite sorted yet

---
+++ src/com/hieblmi/basics/SortArrays.java
@@ -0,0 +1,28 @@
+package com.hieblmi.basics;
+
+import java.util.Arrays;
+
+public class SortArrays {
+    public static void main(String[] args) {
+
+        int[] a = {1, 2};
+        int[] b = {2, 3};
+        int[] c = {3, 5};
+        int[] d = new int[a.length + b.length + c.length];
+
+        int i,j,k,l;
+
+        i=j=k=l=0;
+
+        for(;l<d.length; l++) {
+            int min = Math.min(Math.min(a[i], b[j]), c[k]);
+            if(a[i] == min) i++;
+            if(b[j] == min) j++;
+            if(c[k] == min) k++;
+            d[l] = min;
+        }
+
+        System.out.printf(Arrays.toString(d));
+
+    }
+}
