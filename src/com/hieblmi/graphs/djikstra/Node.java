7d1bbe616bcd2b3f82c059f7f697001c167d54ed
hieblmi <hieblmi@gmail.com>
2017-08-08 15:48:21 -0400

    Began Djikstra with priority queue

--- src/com/hieblmi/graphs/djikstra/Node.java
+++ src/com/hieblmi/graphs/djikstra/Node.java
@@ -1,4 +1,31 @@
 package com.hieblmi.graphs.djikstra;
 
 public class Node {
+    private boolean visited;
+    private int dist;
+    private Node prev;
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public int getDist() {
+        return dist;
+    }
+
+    public void setDist(int dist) {
+        this.dist = dist;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
 }
