a8fa33ba273ed6c9ac8cd9f8deee44ac7be5a01d
hieblmi <hieblmi@gmail.com>
2017-08-22 11:39:01 -0400

    Reverse linked list in place

---
+++ src/com/hieblmi/basics/ReverseLinkedList.java
@@ -0,0 +1,20 @@
+package com.hieblmi.basics;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*Node Reverse(Node head) {
+        Node newNext = null;
+        Node newPrev = head;
+        while(newPrev != null) {
+            Node tmp = newPrev.next;
+            newPrev.next = newNext;
+            newNext = newPrev;
+            newPrev = tmp;
+        }
+        return newNext;
+    }*/
+}
